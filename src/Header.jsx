674590c73274b6edb11a9d2756e2569febdd2092
QUARTZON <katipogumohan10@gmail.com>
2026-06-29 11:48:06 +0530

    Updated Quartzon website

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,56 +1,80 @@
+import { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
+import { FaPhoneAlt } from "react-icons/fa";
 import "./Header.css";
 import logo from "./image.png";
-import Iamgeslider from "./Imageslider"
-import { FaPhoneAlt } from "react-icons/fa";
-import About from "./About";
-import Contact from "./Contact";
-import ReadMore from "./Readmore";
+
 function Header() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <Navbar expand="lg" className="quarzon-navbar" sticky="top">
+    <Navbar
+      expand="lg"
+      fixed="top"
+      collapseOnSelect
+      className={`quarzon-navbar ${scrolled ? "scrolled" : ""}`}
+    >
       <Container fluid>
 
         {/* Logo */}
         <Navbar.Brand as={Link} to="/">
-        <img
-         src={logo}
-         alt="Quartzon"
-         className="navbar-logo" 
-        />
-         </Navbar.Brand>
+          <img
+            src={logo}
+            alt="Quartzon"
+            className="navbar-logo"
+          />
+        </Navbar.Brand>
 
         {/* Mobile Toggle */}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
         {/* Menu */}
         <Navbar.Collapse id="basic-navbar-nav">
+
           <Nav className="mx-auto nav-links">
-            <Nav.Link as={Link} to="/" className="active-link">
-             HOME
+
+            <Nav.Link as={Link} to="/">
+              HOME
             </Nav.Link>
+
             <Nav.Link as={Link} to="/about">
-            ABOUT
+              ABOUT
             </Nav.Link>
 
             <Nav.Link as={Link} to="/services">
-            SERVICES
+              SERVICES
             </Nav.Link>
 
             <Nav.Link as={Link} to="/readmore">
-             READMORE
+              READ MORE
             </Nav.Link>
 
             <Nav.Link as={Link} to="/contact">
-            CONTACT
+              CONTACT
             </Nav.Link>
+
           </Nav>
 
-          {/* Phone */}
-          <a href="tel:+12345678900" className="phone-btn">
+          <a href="tel:+919701555559" className="phone-btn">
             <FaPhoneAlt className="me-2" />
             +91 9701555559
           </a>
